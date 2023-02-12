cbc1b9016a6a8e1b279bc38e3762fd1e1b77c494
Xavi <email@javier-rodriguez.com>
2023-02-12 19:19:22 +0100

    first Commit

---
+++ src/test/java/com/example/sprinbootzk/SprinbootZkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootzk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootZkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
